a88c20bc12604092b033893ccbfcc12821f89aa1
nmegrant <nme.grant@gmail.com>
2020-08-28 23:37:55 +0200

    Change column title

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, MouseEvent, FormEvent } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 
@@ -13,12 +14,37 @@ const useStyles = makeStyles((theme: any) => ({
 
 export default function Column() {
   const classes = useStyles();
+  const [editTitle, setEditTitle] = useState(false);
+  const [title, setTitle] = useState("Title");
+
+  function setToEdit(event: MouseEvent) {
+    event.preventDefault();
+    setEditTitle(true);
+  }
+
+  function setToNotEdit(event: FormEvent) {
+    event.preventDefault();
+    setEditTitle(false);
+  }
+
   return (
     <Grid container>
       <Grid item xs={12}>
         <Grid container justify="center">
           <Grid>
-            <Paper className={classes.paper} />
+            <Paper className={classes.paper}>
+              {editTitle ? (
+                <form onSubmit={setToNotEdit}>
+                  <TextField
+                    id="standard-basic"
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                  />
+                </form>
+              ) : (
+                <header onClick={setToEdit}>{title}</header>
+              )}
+            </Paper>
           </Grid>
         </Grid>
       </Grid>
