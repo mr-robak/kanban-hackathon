6d7f74d0d0d89c7e2a03c4b21ea6d69a72db8aae
Marcin Robak <mr.robak@gmail.com>
2020-08-28 20:25:28 +0200

    components templates  Board, Columns

---
+++ src/components/Board.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Column from "./Column";
+
+export default function Board() {
+  return (
+    <div>
+      <Column />
+      <Column />
+      <Column />
+    </div>
+  );
+}
