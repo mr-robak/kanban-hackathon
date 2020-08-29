cb6d3e8542ea2bafe597ad14aa0310c8d0b2c409
contexD <maxconte232@gmail.com>
2020-08-29 18:11:58 +0200

    Import saveState

--- src/state/reducer.tsx
+++ src/state/reducer.tsx
@@ -1,17 +1,22 @@
 import { Action, State } from "../models/index";
+import { saveState } from "../utils/index";
 
 export default function reducer(state: State, action: Action) {
   switch (action.type) {
     case "addColumn": {
+      //call saveState(newState) to save to local storage
       return state;
     }
     case "deleteColumn": {
+      //call saveState(newState) to save to local storage
       return state;
     }
     case "addCard": {
+      //call saveState(newState) to save to local storage
       return state;
     }
     case "deleteCard": {
+      //call saveState(newState) to save to local storage
       return state;
     }
     default: {
