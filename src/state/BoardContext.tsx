ee121f386fd82c5de2d45180ac8d7ca2512440a8
contexD <maxconte232@gmail.com>
2020-08-29 17:39:41 +0200

    Set initialState to null

--- src/state/BoardContext.tsx
+++ src/state/BoardContext.tsx
@@ -1,23 +1,7 @@
 import React, { createContext } from "react";
 import { State, ContextValue } from "../models/index";
 
-export const initialState: State = [
-  {
-    id: 1,
-    name: "To do",
-    cards: [{ id: 1, title: "Make coffee", description: "" }],
-  },
-  {
-    id: 2,
-    name: "In Progress",
-    cards: [{ id: 2, title: "Build kanban board", description: "" }],
-  },
-  {
-    id: 3,
-    name: "Done",
-    cards: [{ id: 3, title: "Found awesome team", description: "" }],
-  },
-];
+const initialState = null;
 
 const BoardContext = createContext<ContextValue>(null);
 
