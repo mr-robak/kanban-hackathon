be8374cb607f04639ad3478c3225b845b5a85a4f
contexD <maxconte232@gmail.com>
2020-08-29 19:10:58 +0200

    Add useEffect to save state in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useReducer } from "react";
+import React, { useEffect, useMemo, useReducer } from "react";
 import BoardContext, { initialState } from "./state/BoardContext";
 import reducer from "./state/reducer";
 import "./App.css";
@@ -6,9 +6,16 @@ import Board from "./components/Board";
 import LandingPage from "./pages/LandingPage";
 import Navbar from "./components/Navbar";
 import { Switch, Route } from "react-router-dom";
+import { saveState } from "./utils";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log("state", state);
+
+  useEffect(() => {
+    saveState(state);
+  }, [state]);
 
   const contextValue = useMemo(() => {
     return { state, dispatch };
