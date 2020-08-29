64d8b9c1e98bf9bd5c91355dcfb56c965f39a950
contexD <maxconte232@gmail.com>
2020-08-29 10:01:50 +0200

    Wrap board with BoardContext provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext, useMemo, useReducer } from "react";
+import BoardContext, { initialState } from "./state/BoardContext";
+import reducer from "./state/reducer";
 import "./App.css";
 
 import Board from "./components/Board";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const contextValue = useMemo(() => {
+    return { state, dispatch };
+  }, [state, dispatch]);
+
   return (
     <div className="App" style={{ background: "#b19cd9", height: 1000 }}>
-       
-      <Board />
+      <BoardContext.Provider value={contextValue}>
+        <Board />
+      </BoardContext.Provider>
     </div>
   );
 }
