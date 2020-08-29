4b84b11a2291f40f6b3eb2c9f6fded723386999b
contexD <maxconte232@gmail.com>
2020-08-29 17:53:14 +0200

    Remove unnecessary imports

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,6 @@
 import React, { MouseEvent, useState } from "react";
 import Column from "./Column";
 import Button from "@material-ui/core/Button";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: any) => ({
