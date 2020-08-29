30c8841ff9222cbcb1a0aa2c6611bc7e2786a3ac
contexD <maxconte232@gmail.com>
2020-08-29 09:54:55 +0200

    Import Action and State into reducer

--- src/state/reducer.tsx
+++ src/state/reducer.tsx
@@ -1,24 +1,21 @@
-interface Action {
-    type: string,
-    payload: any,
-}
+import { Action, State } from "../models/index";
 
-export default function reducer(state: Array<Column>, action: Action) {
-    switch(action.type) {
-        case "addColumn": {
-            return state;
-        };
-        case "deleteColumn": {
-            return state;
-        };
-        case "addCard": {
-            return state;
-        };
-        case "deleteCard": {
-            return state;
-        };
-        default: {
-            return state;
-        }
+export default function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case "addColumn": {
+      return state;
     }
+    case "deleteColumn": {
+      return state;
+    }
+    case "addCard": {
+      return state;
+    }
+    case "deleteCard": {
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
 }
