930bfb2b722b5ddd1d7f3c38322d31b418e44d87
Marcin Robak <mr.robak@gmail.com>
2020-08-29 07:23:45 +0200

    landing page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-
-import Board from "./components/Board";
+// import Board from "./components/Board";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App" style={{ background: "#b19cd9", height: 1000 }}>
-       
-      <Board />
+        <LandingPage />
     </div>
   );
 }
