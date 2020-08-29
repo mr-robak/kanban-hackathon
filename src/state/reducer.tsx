c5e01f8751b0c3773eeeae5a5dd5b9bca871eb1b
contexD <maxconte232@gmail.com>
2020-08-29 19:09:49 +0200

    Remove console logs and saveState

--- src/state/reducer.tsx
+++ src/state/reducer.tsx
@@ -1,22 +1,17 @@
 import { Action, State } from "../models/index";
-import { saveState } from "../utils/index";
 
 export default function reducer(state: State, action: Action) {
   switch (action.type) {
     case "addColumn": {
-      //call saveState(newState) to save to local storage
       return state;
     }
     case "deleteColumn": {
-      //call saveState(newState) to save to local storage
       return state;
     }
     case "addCard": {
-      //call saveState(newState) to save to local storage
       return state;
     }
     case "deleteCard": {
-      //call saveState(newState) to save to local storage
       return state;
     }
     default: {
