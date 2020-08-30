4702bca9c1759bec194a38fac8dea026ad90bb96
Marcin Robak <mr.robak@gmail.com>
2020-08-30 19:44:12 +0200

    lint cleant

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
 
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import { NavLink, Link } from "react-router-dom";
 
 import "./Navbar.css";
