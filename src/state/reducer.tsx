4d4829e2f00557d51c2d1d6022c5b06857d98bb0
contexD <maxconte232@gmail.com>
2020-08-30 06:34:05 +0200

    Add state update to 'deleteTask' action

--- src/state/reducer.tsx
+++ src/state/reducer.tsx
@@ -1,4 +1,4 @@
-import { Action, State } from "../models/index";
+import { Action, State, Tasks } from "../models/index";
 
 export default function reducer(state: State, action: Action) {
   switch (action.type) {
@@ -12,14 +12,23 @@ export default function reducer(state: State, action: Action) {
       return state;
     }
     case "deleteTask": {
-      //const { id } = action.payload;
-      // const newState = {};
-      // for(const key in state.tasks) {
-      //   if(key !== id) {
-      //     newState =
-      //   }
-      // }
-      return state;
+      const id = action.payload;
+      const newTasks: Tasks = {};
+      for (const key in state.tasks) {
+        if (key !== id) {
+          newTasks[key] = state.tasks[key];
+        }
+      }
+      //update columns
+      const newColumns = { ...state.columns };
+      for (const key in newColumns) {
+        newColumns[key].taskIds = newColumns[key].taskIds.filter(
+          (taskId) => taskId !== id
+        );
+      }
+      console.log("new tasks", newTasks);
+      console.log("new columns", newColumns);
+      return { ...state, columns: newColumns, tasks: newTasks };
     }
     default: {
       return state;
