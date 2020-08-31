435c1259ff0d67963947291f84a18b633ae0cc85
contexD <maxconte232@gmail.com>
2020-08-31 02:54:44 +0200

    Comment unused imports and vars

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -12,11 +12,11 @@ import Panorama from "@material-ui/icons/Panorama";
 import RotateLeft from "@material-ui/icons/RotateLeft";
 import EditIcon from "@material-ui/icons/Edit";
 
-import AddIcon from "@material-ui/icons/Add";
-import Fab from "@material-ui/core/Fab";
-// import DeleteIcon from '@material-ui/icons/Delete';
-// import IconButton from '@material-ui/core/IconButton';
-import Tooltip from "@material-ui/core/Tooltip";
+// import AddIcon from "@material-ui/icons/Add";
+// import Fab from "@material-ui/core/Fab";
+// // import DeleteIcon from '@material-ui/icons/Delete';
+// // import IconButton from '@material-ui/core/IconButton';
+// import Tooltip from "@material-ui/core/Tooltip";
 
 const useStyles = makeStyles((theme: any) => ({
   root: {
