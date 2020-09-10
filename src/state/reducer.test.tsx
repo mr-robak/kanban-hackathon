a78754af2bc69d7ccef8031db9b7b977352eae40
nmegrant <nme.grant@gmail.com>
2020-09-10 15:07:40 +0200

    Test to ensure initial state returned if action invalid

--- src/state/reducer.test.tsx
+++ src/state/reducer.test.tsx
@@ -2,6 +2,15 @@ import reducer from "./reducer";
 import { initialState } from "./BoardContext";
 
 describe("reducer", () => {
+  describe("if given an invalid action type", () => {
+    test("return initial state", () => {
+      const newState = reducer(initialState, {
+        type: "multuplyColumn",
+        payload: null,
+      });
+      expect(newState).toEqual(initialState);
+    });
+  });
   describe("if given action type addColumn", () => {
     test("return state with new column added", () => {
       const newColumns = {
